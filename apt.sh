17ee423c3d1314112ec19c4028d4ee381d8eac84
Luke Reid <luke@reid.org.nz>
2016-10-03 12:31:20 +1300

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,6 +1,6 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 073D307A618E5811
 
-echo deb     http://ppa.launchpad.net/ubuntugis/ubuntugis-unstable/ubuntu *codename* main >> /etc/apt/sources.list
+echo deb     http://ppa.launchpad.net/ubuntugis/ubuntugis-unstable/ubuntu xenial main >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install git terminator emacs qgis python-qgis qgis-plugin-grass postgis build-essential ruby 
 
