59c879767bd44755cdd0c1cf458e3543977cb2d0
Luke Reid <luke@reid.org.nz>
2017-05-29 18:57:55 +1200

    mutt

--- apt.sh
+++ apt.sh
@@ -1,3 +1,7 @@
+git config --global user.email "luke@reid.org.nz"
+git config --global user.name "Luke Reid"
+
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 073D307A618E5811
 
 echo deb     http://ppa.launchpad.net/ubuntugis/ubuntugis-unstable/ubuntu xenial main >> /etc/apt/sources.list
